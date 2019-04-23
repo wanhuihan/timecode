623a6f7d7b4a57004e351d30bf5e39af2c70a148
Jack Han <79611225@qq.com>
2019-04-23 18:01:19 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- {{timeCodeData}} -->
+    <vue-good-table
+      :columns = "tableHead"
+      :rows = "timeCodeData" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { VueGoodTable } from 'vue-good-table';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    VueGoodTable
+  },
+  data () {
+    return {
+      timeCodeData: [],
+      tableHead: [
+        {
+          label: 'Chinese Name',
+          field: 'ChineseName'
+        }, {
+          label: 'English Name',
+          field: 'EnglishName'
+        }, {
+          label: 'Department',
+          field: 'department'
+        }, {
+          label: 'Task Content',
+          field: 'Task'
+        }, {
+          label: 'Id',
+          field: '_id'
+        }
+      ]
+    }
+  },
+  created () {
+    // console.log(this.axios)
+    this.axios.get('http://10.2.178.174:8001/user').then( data => {
+
+      this.timeCodeData = data.data.data;
+      console.log(this.timeCodeData)
+    })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import axios from 'axios';
+import 'vue-good-table/dist/vue-good-table.css'
 Vue.config.productionTip = false
-
+// import VueGoodTablePlugin from 'vue-good-table';
+// Vue.use(VueGoodTablePlugin);
+Vue.prototype.axios = axios;
 new Vue({
   render: h => h(App),
 }).$mount('#app')
